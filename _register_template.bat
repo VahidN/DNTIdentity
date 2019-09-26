581575ddea051f41004fb96befa282f68c0dd743
VahidN <vahid_nasiri@yahoo.com>
2019-09-26 20:56:44 +0330

    update _register_template.bat

--- _register_template.bat
+++ _register_template.bat
@@ -1,4 +1,7 @@
-dotnet new -i %~dp0
+SET DNTPath=%~dp0
+IF "%DNTPath:~-1%"=="\" SET DNTPath=%DNTPath:~0,-1%
+dotnet new -u %DNTPath%
+dotnet new -i %DNTPath%
 dotnet new --list
 rem dotnet new dntidentity -n MyNewProj
 pause
