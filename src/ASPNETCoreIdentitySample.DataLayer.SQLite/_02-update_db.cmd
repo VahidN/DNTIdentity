0a924ddeac18c411a2ef19d37014049732ca2daa
VahidN <vahid_nasiri@yahoo.com>
2024-03-02 12:59:57 +0330

    Update ef-tools version

--- src/ASPNETCoreIdentitySample.DataLayer.SQLite/_02-update_db.cmd
+++ src/ASPNETCoreIdentitySample.DataLayer.SQLite/_02-update_db.cmd
@@ -1,4 +1,4 @@
-dotnet tool update --global dotnet-ef --version 7.0.8
+dotnet tool update --global dotnet-ef --version 8.0.2
 dotnet tool restore
 dotnet build
 dotnet ef --startup-project ../ASPNETCoreIdentitySample/ database update --context SQLiteDbContext
