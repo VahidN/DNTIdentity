1459c3dd97d679643eab9a79b98fd46c8f19fac0
VahidN <vahid_nasiri@yahoo.com>
2025-02-08 23:20:29 +0330

    Use an old version of LigerShark.WebOptimizer.Core to fix relative path issues

--- src/ASPNETCoreIdentitySample.IocConfig/WebOptimizerRegistry.cs
+++ src/ASPNETCoreIdentitySample.IocConfig/WebOptimizerRegistry.cs
@@ -5,36 +5,29 @@ namespace ASPNETCoreIdentitySample.IocConfig;
 public static class WebOptimizerRegistry
 {
     public static void AddWebOptimizerServices(this IServiceCollection services)
-    {
-        services.AddWebOptimizer(pipeline =>
-                                 {
-                                     // Creates a CSS and a JS bundle. Globbing patterns supported.
-                                     pipeline.AddCssBundle("/css/site.min.css",
-                                                           "wwwroot/lib/bootstrap/dist/css/bootstrap.min.css",
-                                                           "wwwroot/lib/bootstrap4-rtl/bootstrap-rtl.css",
-                                                           "wwwroot/lib/components-font-awesome/css/solid.min.css",
-                                                           "wwwroot/lib/components-font-awesome/css/fontawesome.min.css",
-                                                           "wwwroot/content/custom.css"
-                                                          )
-                                             .UseContentRoot()
-                                             .AdjustRelativePaths();
-                                     pipeline.AddJavaScriptBundle("/js/site.min.js",
-                                                                  "wwwroot/lib/jquery/dist/jquery.min.js",
-                                                                  "wwwroot/lib/popperjs/dist/umd/popper.min.js",
-                                                                  "wwwroot/lib/jquery-validation/dist/jquery.validate.min.js",
-                                                                  "wwwroot/lib/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js",
-                                                                  "wwwroot/lib/jquery-ajax-unobtrusive/dist/jquery.unobtrusive-ajax.min.js",
-                                                                  "wwwroot/lib/bootstrap/dist/js/bootstrap.min.js",
-                                                                  "wwwroot/scripts/jquery.bootstrap-modal-confirm.js",
-                                                                  "wwwroot/scripts/jquery.bootstrap-modal-alert.js",
-                                                                  "wwwroot/scripts/jquery.bootstrap-modal-ajax-form.js",
-                                                                  "wwwroot/scripts/custom.js"
-                                                                 )
-                                             .UseContentRoot();
+        => services.AddWebOptimizer(pipeline =>
+        {
+            // Creates a CSS and a JS bundle. Globbing patterns supported.
+            pipeline.AddCssBundle(route: "/css/site.min.css", "wwwroot/lib/bootstrap/dist/css/bootstrap.min.css",
+                    "wwwroot/lib/bootstrap4-rtl/bootstrap-rtl.css",
+                    "wwwroot/lib/components-font-awesome/css/solid.min.css",
+                    "wwwroot/lib/components-font-awesome/css/fontawesome.min.css", "wwwroot/content/custom.css")
+                .AdjustRelativePaths()
+                .UseContentRoot();
 
-                                     // This will minify any JS and CSS file that isn't part of any bundle
-                                     pipeline.MinifyCssFiles();
-                                     pipeline.MinifyJsFiles();
-                                 });
-    }
+            pipeline.AddJavaScriptBundle(route: "/js/site.min.js", "wwwroot/lib/jquery/dist/jquery.min.js",
+                    "wwwroot/lib/popperjs/dist/umd/popper.min.js",
+                    "wwwroot/lib/jquery-validation/dist/jquery.validate.min.js",
+                    "wwwroot/lib/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js",
+                    "wwwroot/lib/jquery-ajax-unobtrusive/dist/jquery.unobtrusive-ajax.min.js",
+                    "wwwroot/lib/bootstrap/dist/js/bootstrap.min.js",
+                    "wwwroot/scripts/jquery.bootstrap-modal-confirm.js",
+                    "wwwroot/scripts/jquery.bootstrap-modal-alert.js",
+                    "wwwroot/scripts/jquery.bootstrap-modal-ajax-form.js", "wwwroot/scripts/custom.js")
+                .UseContentRoot();
+
+            // This will minify any JS and CSS file that isn't part of any bundle
+            pipeline.MinifyCssFiles();
+            pipeline.MinifyJsFiles();
+        });
 }
