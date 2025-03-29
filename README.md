4f623e67ddd409dadc184ef4ac70384ac327110a
VahidN <vahid_nasiri@yahoo.com>
2025-03-29 13:39:12 +0330

    Update dependencies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ﻿<div dir="rtl">
 
-# سفارشی سازی ASP.NET Core Identity SDK-9.0.100
+# سفارشی سازی ASP.NET Core Identity SDK-9.0.201
 
 <p>
   <a href="https://github.com/VahidN/DNTIdentity">
--- src/ASPNETCoreIdentitySample.DataLayer.SQLite/_02-update_db.cmd
+++ src/ASPNETCoreIdentitySample.DataLayer.SQLite/_02-update_db.cmd
@@ -1,4 +1,4 @@
-dotnet tool update --global dotnet-ef --version 9.0.0
+dotnet tool update --global dotnet-ef --version 9.0.3
 dotnet tool restore
 dotnet build
 dotnet ef --startup-project ../ASPNETCoreIdentitySample/ database update --context SQLiteDbContext
